5bd8d99cb464f020aadde1d9d4d9e83387fa2799
jpashley1 <jpashley1@gmail.com>
2024-09-16 21:38:05 -0400

    Added Babel

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 /* global moment */
-var moment = require('moment'); // require
-import { sqrt } from 'mathjs';
-
+var moment = require("moment"); // require
+import { sqrt } from "mathjs";
 
 console.log("heyyyyy dude");
 
-var datetime = moment().format("MMM Do YY");               // Sep 16th 24
+var datetime = moment().format("MMM Do YY"); // Sep 16th 24
 console.log(datetime);
-
 
 console.log(sqrt(25).toString()); // 2i
 
+
+var name = "Bob", time = "today";
+console.log(`Hello ${name}, how are you ${time}?`);

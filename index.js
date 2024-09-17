2012b719bfa9bf741a553d57af8f19c39277fc00
jpashley1 <jpashley1@gmail.com>
2024-09-16 21:11:30 -0400

    added webpack config

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ var moment = require('moment'); // require
 import { sqrt } from 'mathjs';
 
 
-
 console.log("heyyyyy dude");
 
 var datetime = moment().format("MMM Do YY");               // Sep 16th 24
@@ -11,3 +10,4 @@ console.log(datetime);
 
 
 console.log(sqrt(25).toString()); // 2i
+

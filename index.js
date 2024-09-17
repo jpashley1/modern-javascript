f2c3536ea3bdff30230fd84d3af4ff64f6bc3225
jpashley1 <jpashley1@gmail.com>
2024-09-16 20:47:25 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+/* global moment */
+var moment = require('moment'); // require
+import { sqrt } from 'mathjs';
+
+
+
+console.log("heyyyyy dude");
+
+var datetime = moment().format("MMM Do YY");               // Sep 16th 24
+console.log(datetime);
+
+
+console.log(sqrt(25).toString()); // 2i
